be12a3867be8dffadc9c7d5b7610943a98e4bd50
Sunit Chandra <sunitorchandra@gmail.com>
2023-04-27 22:20:10 +0530

    adding the ssl connect

--- index.php
+++ index.php
@@ -4,10 +4,29 @@
  </head>
  <body>
  <?php echo '<p>Hello World</p>'; 
- $servername = "mysqltestsc.mysql.database.azure.com";
+	$servername = "mysqltestsc.mysql.database.azure.com";
 	$username = "azureadmin";
 	$password = "Rockwell123!@#";
+	$db_name = "testdb";
 
+	//Initializes MySQLi
+	$conn = mysqli_init();
+
+	mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/DigiCertGlobalRootG2.crt.pem", NULL, NULL);
+
+	// Establish the connection 'myadmin@mydemoserver'
+	mysqli_real_connect($conn, $servername, $username, $password, $db_name, 3306, NULL, MYSQLI_CLIENT_SSL);
+
+	//If connection failed, show the error
+	if (mysqli_connect_errno())
+	{
+		die('Failed to connect to MySQL: '.mysqli_connect_error());
+	}
+	else {
+		echo "Connected Sucessfully..!!";
+	}
+	
+	/*
 	// Create connection
 	$conn = new mysqli($servername, $username, $password);
 
@@ -16,6 +35,20 @@
 	  die("Connection failed: " . $conn->connect_error);
 	}
 	echo "Connected successfully";
+	
+	
+	CREATE DATABASE testdb;
+
+	CREATE USER 'db_user'@'%' IDENTIFIED BY 'StrongPassword!';
+
+	GRANT ALL PRIVILEGES ON testdb . * TO 'db_user'@'%';
+
+	FLUSH PRIVILEGES;
+
+	USE testdb;
+
+	SHOW GRANTS FOR 'db_user'@'%';
+	*/
 ?>
  </body>
 </html>
