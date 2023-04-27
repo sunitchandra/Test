412768916d4c85713675fa612f633960482a1e86
Sunit Chandra <sunitorchandra@gmail.com>
2023-04-27 22:22:41 +0530

    hello world 1

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World</p>'; 
+ <?php echo '<p>Hello World 1</p>'; 
 	$servername = "mysqltestsc.mysql.database.azure.com";
 	$username = "azureadmin";
 	$password = "Rockwell123!@#";
