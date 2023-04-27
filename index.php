d03d945932f14a54d49ea8c23658856dc5034948
Sunit Chandra <sunitorchandra@gmail.com>
2023-04-27 22:27:21 +0530

    create table, insert data, read data

--- index.php
+++ index.php
@@ -24,6 +24,39 @@
 	}
 	else {
 		echo "Connected Sucessfully..!!";
+		
+		// Run the create table query
+		if (mysqli_query($conn, '
+		CREATE TABLE Products (
+		`Id` INT NOT NULL AUTO_INCREMENT ,
+		`ProductName` VARCHAR(200) NOT NULL ,
+		`Color` VARCHAR(50) NOT NULL ,
+		`Price` DOUBLE NOT NULL ,
+		PRIMARY KEY (`Id`)
+		);
+		')) {
+		printf("Table created\n");
+		}
+		
+		//Create an Insert prepared statement and run it
+		$product_name = 'BrandNewProduct';
+		$product_color = 'Blue';
+		$product_price = 15.5;
+		if ($stmt = mysqli_prepare($conn, "INSERT INTO Products (ProductName, Color, Price) VALUES (?, ?, ?)"))
+		{
+			mysqli_stmt_bind_param($stmt, 'ssd', $product_name, $product_color, $product_price);
+			mysqli_stmt_execute($stmt);
+			printf("Insert: Affected %d rows\n", mysqli_stmt_affected_rows($stmt));
+			mysqli_stmt_close($stmt);
+		}
+		
+		//Run the Select query
+		printf("Reading data from table: \n");
+		$res = mysqli_query($conn, 'SELECT * FROM Products');
+		while ($row = mysqli_fetch_assoc($res))
+		 {
+			var_dump($row);
+		 }
 	}
 	
 	/*
